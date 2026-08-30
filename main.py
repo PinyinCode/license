34c3024682b8a9a0093d539726a7c5f55ed6ac94
PinyinCode <160508972+PinyinCode@users.noreply.github.com>
2026-08-30 22:49:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ GITHUB_CLIENT_SECRET = "158a74d6beed0ed201ad9a7c4a041738d3185eb6"
 YOUR_GITHUB_USERNAME = "PinyinCode"
 
 # Link file firmware .bin của bạn trên Render Static Site
-DEFAULT_FIRMWARE_URL = "https://esp32-z1t9.onrender.com/xiaozhi.bin"
+DEFAULT_FIRMWARE_URL = "https://esp32-linkdownload.onrender.com/xiaozhi.bin"
 DEFAULT_LATEST_VERSION = "v1.1.0"
 
 
